4670e3b3d97c3ed378d8a68a264eed56163940f1
Sorbopriyo Roy <sorbopriyo@gmail.com>
2024-11-13 09:51:19 +0530

    Get real-time followers of the creators

--- TODO.md
+++ TODO.md
@@ -1,11 +1,12 @@
 # Next things to do
 
 1. [DONE]Get real data of the creators from a database
-2. Get real-time followers of the creators
-3. Get real data of the contributors from a database
-4. A button to join the Discord server
-5. A way to contribute for the server in the landing page
-6. Make the build page
-7. Servers page
-8. Community - Forum, logged in users will be able to discuss different things in different threads, these threads will support normal messages, emojis, photos
-9. Store - Godcraft's marketplace where people will be able to buy server credits, builds, memberships or they can contribute
+2. [DONE]Get real-time followers of the creators
+3. Implement Display image of the creator using YouTube data api v3
+4. Get real data of the contributors from a database
+5. A button to join the Discord server
+6. A way to contribute for the server in the landing page
+7. Make the build page
+8. Servers page
+9. Community - Forum, logged in users will be able to discuss different things in different threads, these threads will support normal messages, emojis, photos
+10. Store - Godcraft's marketplace where people will be able to buy server credits, builds, memberships or they can contribute
--- src/components/TopCreators.tsx
+++ src/components/TopCreators.tsx
@@ -15,12 +15,47 @@ interface Creator {
     };
 }
 
+async function extractChannelId(url: string): Promise<string | null> {
+    const urlObj = new URL(url);
+    const pathParts = urlObj.pathname.split('/');
+
+    // Check for common URL formats and return the ID if directly available
+    if (pathParts.includes("channel")) {
+        return pathParts[pathParts.indexOf("channel") + 1]; // Channel ID is the next part in URL
+    } else if (pathParts.includes("user") || pathParts[1].startsWith('@')) {
+        // If we have a custom username (like @username or /user/username), use the API to get the channel ID
+        const username = pathParts.includes("user") ? pathParts[pathParts.indexOf("user") + 1] : pathParts[1].substring(1);
+        return username;
+    }
+
+    // Return null if the URL does not match any recognizable YouTube channel URL patterns
+    return null;
+}
+
+
+async function fetchFollowersOfCreator(channelHandle: string): Promise<number> {
+    const response = await fetch(`https://www.googleapis.com/youtube/v3/channels?forHandle=${channelHandle}&part=statistics&key=${process.env.YOUTUBE_DATA_API_KEY}`);
+
+    if (!response) {
+        throw new Error('Failed to fetch followers of creator');
+    }
+
+    const data = await response.json();
+
+    return data.items[0].statistics.subscriberCount;
+}
+
 async function fetchCreators(): Promise<Creator[]> {
     const response = await axios.get('http://localhost:3000/api/fetchCreators')
     if (!response) {
         throw new Error('Failed to fetch creators');
     }
     const creatorsData = await response.data;
+
+    for (let i = 0; i < creatorsData.length; i++) {
+        creatorsData[i].followers = await fetchFollowersOfCreator(await extractChannelId(creatorsData[i].socials.youtube) as string);
+    }
+
     return creatorsData;
 }
 
@@ -36,7 +71,7 @@ const TopCreators = async () => {
                         <Image src={creator.image} alt={creator.name} width={160} height={160} className='w-40 h-40 object-cover rounded-full' />
                         <h3 className='text-xl font-semibold'>{creator.name}</h3>
                         <p className='text-gray-400 text-sm'>{creator.tags.join(', ')}</p>
-                        <p className='text-gray-400 text-sm'>Followers: <span className='text-white border-2 border-gray-600 rounded-full px-2'>{181}</span></p>
+                        <p className='text-gray-400 text-sm'>Followers: <span className='text-white border-2 border-gray-600 rounded-full px-2'>{creator.followers}</span></p>
                         <div className='flex gap-2 items-center justify-center'>
                             <Link href={creator.socials.youtube} target='_blank' rel='noopener noreferrer'>
                                 <Image src="/assets/youtube.png" alt="YouTube" width={24} height={24} className='h-6 object-contain' />
