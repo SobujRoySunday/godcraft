05863b863213a3fd00d29cd4cfd63977cbf1d9fa
Sorbopriyo Roy <sorbopriyo@gmail.com>
2024-11-13 10:04:01 +0530

    Fix: eslint needs to be smart

--- src/components/TopCreators.tsx
+++ src/components/TopCreators.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Container from './Container'
 import Image from 'next/image'
 import Link from 'next/link'
-import axios from 'axios'
 
 interface Creator {
     name: string;
